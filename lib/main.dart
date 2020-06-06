76c03bfc66b58b26507a823580f3e88d6cb9a978
leopalmeiro <leonardo.palmeiro@babel.es>
2020-06-06 17:14:13 +0200

    add title on scaffold from widget attributes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.black,
         backgroundColor: Colors.white,
@@ -30,7 +29,7 @@ class MyApp extends StatelessWidget {
               color: Colors.grey, fontSize: 15, fontWeight: FontWeight.normal),
         ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Profile Challenge 03'),
     );
   }
 }
@@ -38,14 +37,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,16 +47,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Profile Challenge 03'),
+        title: Text(widget.title),
       ),
       body: SingleChildScrollView(
         child: Stack(
